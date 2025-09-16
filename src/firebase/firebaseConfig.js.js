5d8a4e4b4baad39d78635f48af7863f4996e5301
David4988 <davidson4988@gmail.com>
2025-09-16 19:32:10 +0530

    Firebase config

---
+++ src/firebase/firebaseConfig.js.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB01IWrvE4L8EoxhXrdESKm1dstD_gH8Y8",
+  authDomain: "project-sentinel-4988.firebaseapp.com",
+  projectId: "project-sentinel-4988",
+  storageBucket: "project-sentinel-4988.firebasestorage.app",
+  messagingSenderId: "885330481939",
+  appId: "1:885330481939:web:b3186952a0cd5b2b3fc0e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
